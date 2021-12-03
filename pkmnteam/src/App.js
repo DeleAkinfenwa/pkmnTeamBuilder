c7d62403f53d6a63cfa0447c291b86fdbcc66bdd
Akindele Akinfenwa <Dele@Akindeles-MacBook-Pro.local>
2021-12-03 11:30:01 -0500

    base framework

--- pkmnteam/src/App.js
+++ pkmnteam/src/App.js
@@ -1,7 +1,28 @@
 
 import './App.css';
+import { getPkmn, getTeams, postTeam } from './services'
+import { useState, useEffect } from 'react'
+// import { Routes, Route } from 'react-router-dom'
+
+
 
 function App() {
+
+  const [pkmn, setPkmn] = useState('')
+  const [team, setTeam] = useState([])
+  const [toggle, setToggle] = useState(false)
+
+  useEffect(() => {
+    // pull data for specific pokemon
+    const pkmnList = async () => {
+      const pokemon = await getPkmn()
+      setPkmn(pokemon)
+      console.log(pokemon)
+    }
+
+  }, [toggle])
+
+
   return (
     <div className="App">
       <h1>Pkmn Team Builder</h1>
---
+++ pkmnteam/src/services/index.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+export const baseURL =
+  `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/recipes`
+export const config = {
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`
+  }
+}
+export const pokeAPI = `https://pokeapi.co/api/v2/pokemon/mawile`
+
+
+export const getPkmn = async () => {
+  const response = await axios.get(pokeAPI)
+  return response
+}
+
+export const getTeams = async () => {
+  const response = await axios.get(baseURL, config)
+  return response.data.records
+}
+
+export const postTeam = async (body) => {
+  const response = await axios.post(baseURL, { fields: body }, config)
+  return response.data
+}
